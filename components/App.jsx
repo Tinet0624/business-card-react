de668cecf0c31cc141f6f8ab4c6136c22eb497b0
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2022-08-01 09:55:02 -0400

    seperated css styles

--- components/App.jsx
+++ components/App.jsx
@@ -3,10 +3,11 @@ import Info from "./Info"
 import About from "./About"
 import Interests from "./Interests"
 import Footer from "./Footer"
+import appCSS from "../styles/App.module.css"
 
 export default function App(){
   return(
-    <div className="container">
+    <div className={appCSS.container}>
       <Info />
       <About />
       <Interests />
--- components/Info.jsx
+++ components/Info.jsx
@@ -2,16 +2,17 @@ import React from "react"
 import photo from "../images/Photo.jpg"
 import emailIcon from "../images/email-icon.png"
 import linkedinIcon from "../images/linkedin-icon.png"
+import infoCSS from "../styles/Info.module.css"
 
 export default function Info(){
     return(
-        <div className="info-wrapper">
-            <img src={photo} className="photo"/>
-            <h1 className="my-name">Alexandra Haley</h1>
-            <p className="title">React Front End Developer</p>
-            <div className="btn-wrapper">
-                <a href = "mailto: alexandrahaley619@gmail.com" target='_blank'><button type="button" className="button"><img src={emailIcon} className="icon"/>Email</button></a>
-                <a href = "https://www.linkedin.com/in/alexandrajhaley/" target='_blank'><button type="button" className="button btn-linkedin"><img src={linkedinIcon} className="icon"/>LinkedIn</button></a>
+        <div className={infoCSS.wrapper}>
+            <img src={photo} className={infoCSS.photo}/>
+            <h1 className={infoCSS.myName}>Alexandra Haley</h1>
+            <p className={infoCSS.title}>React Front End Developer</p>
+            <div className={infoCSS.btnWrapper}>
+                <a href = "mailto: alexandrahaley619@gmail.com" target='_blank'><button type="button" className={infoCSS.button}><img src={emailIcon} className={infoCSS.icon}/>Email</button></a>
+                <a href = "https://www.linkedin.com/in/alexandrajhaley/" target='_blank'><button type="button" className={`${infoCSS.button} ${infoCSS.btnLinkedin}`}><img src={linkedinIcon} className={infoCSS.icon}/>LinkedIn</button></a>
             </div>
         </div>
         
